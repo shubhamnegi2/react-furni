e19b37d6ab3897af303f2d6485c0ff1402b2f5cc
shubhamnegi <shubham.negi@wishfin.com>
2024-07-13 13:05:05 +0530

    sda

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-furni/', // Ensure this matches your repository name
   build: {
-    outDir: 'build'
+    outDir: 'build' // Ensure this matches your deployment directory
   }
 })
